d5c091908f17c9ebb061c06bb28fa6eb65487671
Ashvin Sunga <ashvinsunga@yahooo.com.ph>
2022-06-23 21:06:05 +0800

    redux added on productDetails

--- client/src/action/productActions.js
+++ client/src/action/productActions.js
@@ -3,9 +3,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
-  // PRODUCT_DETAILS_REQUEST,
-  // PRODUCT_DETAILS_SUCCESS,
-  // PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
   // PRODUCT_DELETE_SUCCESS,
   // PRODUCT_DELETE_REQUEST,
   // PRODUCT_DELETE_FAIL,
@@ -45,26 +45,26 @@ export const listProducts = () => async (dispatch) => {
   }
 };
 
-// export const listProductDetails = (id) => async (dispatch) => {
-//   try {
-//     dispatch({ type: PRODUCT_DETAILS_REQUEST });
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
 
-//     const { data } = await axios.get(`/api/products/${id}`);
+    const { data } = await axios.get(`/api/products/${id}`);
 
-//     dispatch({
-//       type: PRODUCT_DETAILS_SUCCESS,
-//       payload: data,
-//     });
-//   } catch (error) {
-//     dispatch({
-//       type: PRODUCT_DETAILS_FAIL,
-//       payload:
-//         error.response && error.response.data.message
-//           ? error.response.data.message
-//           : error.message,
-//     });
-//   }
-// };
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
 // export const deleteProduct = (id) => async (dispatch, getState) => {
 //   try {
--- client/src/reducers/productReducers.js
+++ client/src/reducers/productReducers.js
@@ -2,9 +2,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
-  // PRODUCT_DETAILS_REQUEST,
-  // PRODUCT_DETAILS_SUCCESS,
-  // PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
   // PRODUCT_DELETE_REQUEST,
   // PRODUCT_DELETE_SUCCESS,
   // PRODUCT_DELETE_FAIL,
@@ -43,21 +43,21 @@ export const productListReducer = (state = { products: [] }, action) => {
   }
 };
 
-// export const productDetailsReducer = (
-//   state = { product: { reviews: [] } },
-//   action
-// ) => {
-//   switch (action.type) {
-//     case PRODUCT_DETAILS_REQUEST:
-//       return { ...state, loading: true };
-//     case PRODUCT_DETAILS_SUCCESS:
-//       return { loading: false, product: action.payload };
-//     case PRODUCT_DETAILS_FAIL:
-//       return { loading: false, error: action.payload };
-//     default:
-//       return state;
-//   }
-// };
+export const productDetailsReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { ...state, loading: true };
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload };
+    case PRODUCT_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 // export const productDeleteReducer = (state = {}, action) => {
 //   switch (action.type) {
