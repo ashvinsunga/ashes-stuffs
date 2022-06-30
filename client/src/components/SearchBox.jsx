cf80ce47942dffc572b881edfa50bbe9f24f6dd1
Ashvin Sunga <ashvinsunga@yahooo.com.ph>
2022-06-30 16:55:27 +0800

    Pagination added

--- client/src/components/SearchBox.jsx
+++ client/src/components/SearchBox.jsx
@@ -20,7 +20,7 @@ const SearchBox = ({ navigate }) => {
         name='q'
         onChange={(e) => setKeyword(e.target.value)}
         placeholder='Search Products...'
-        className='mr-sm-2 ml-sm-5'></Form.Control>
+        className='ms-sm-2 ml-sm-5'></Form.Control>
       <Button type='submit' variant='outline-success' className='p-2'>
         Search
       </Button>
