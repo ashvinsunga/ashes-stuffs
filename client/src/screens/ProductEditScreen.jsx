fadc3c74830605993e641e040ab84e72a6c9ca8a
Ashvin Sunga <ashvinsunga@yahooo.com.ph>
2022-07-05 14:21:30 +0800

    edit product tw done

--- client/src/screens/ProductEditScreen.jsx
+++ client/src/screens/ProductEditScreen.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import { Form, Button } from 'react-bootstrap';
+import AppButton from '../components/AppButton';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
@@ -95,11 +95,11 @@ const ProductEditScreen = () => {
 
   return (
     <>
-      <Link to='/admin/productlist' className='btn btn-light my-3'>
-        Go Back
+      <Link to='/admin/productlist'>
+        <AppButton>Go Back</AppButton>
       </Link>
-      <FormContainer>
-        <h1>Edit Product</h1>
+      <div className='tw-grid tw-grid-cols-1 tw-justify-items-center tw-text-lg'>
+        <h1 className='tw-mb-6 tw-font-extrabold'>Edit Product</h1>
         {loadingUpdate && <Loader />}
         {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
         {loading ? (
@@ -107,85 +107,177 @@ const ProductEditScreen = () => {
         ) : error ? (
           <Message variant='danger'>{error}</Message>
         ) : (
-          <Form onSubmit={submitHandler}>
-            <Form.Group controlId='name'>
-              <Form.Label>Name</Form.Label>
-              <Form.Control
-                type='name'
+          <form onSubmit={submitHandler}>
+            <div className='tw-text-left'>
+              <label>Name</label>
+              <br />
+              <input
+                type='text'
                 placeholder='Enter name'
                 value={name}
-                onChange={(e) => setName(e.target.value)}></Form.Control>
-            </Form.Group>
-
-            <Form.Group controlId='price'>
-              <Form.Label>Price</Form.Label>
-              <Form.Control
+                onChange={(e) => setName(e.target.value)}
+                className='tw-w-96 tw-mb-3 tw-px-4 tw-py-2 tw-font-normal tw-text-gray-700 tw-border tw-border-solid tw-border-gray-300 tw-rounded  focus:tw-border-teal-600 focus:tw-outline-none'></input>
+            </div>
+            <div>
+              <label>Price</label>
+              <br />
+              <input
                 type='number'
                 placeholder='Enter price'
                 value={price}
-                onChange={(e) => setPrice(e.target.value)}></Form.Control>
-            </Form.Group>
+                onChange={(e) => setPrice(e.target.value)}
+                className='tw-w-96 tw-mb-3 tw-px-4 tw-py-2 tw-font-normal tw-text-gray-700 tw-border tw-border-solid tw-border-gray-300 tw-rounded  focus:tw-border-teal-600 focus:tw-outline-none'></input>
+            </div>
 
-            <Form.Group controlId='image'>
-              <Form.Label>Image</Form.Label>
-              <Form.Control
-                type='text'
-                placeholder='Enter image url'
-                value={image}
-                onChange={(e) => setImage(e.target.value)}></Form.Control>
-              <Form.Control
-                type='file'
-                // id='image-file'
-                label='Choose File'
-                // custom
-                onChange={uploadFileHandler}></Form.Control>
-              {uploading && <Loader />}
-            </Form.Group>
-
-            <Form.Group controlId='brand'>
-              <Form.Label>Brand</Form.Label>
-              <Form.Control
+            <label>Image</label>
+            <br />
+            <div className='tw-mb-3 tw-border tw-border-solid tw-border-gray-300 tw-rounded'>
+              <div>
+                <input
+                  type='text'
+                  placeholder='Image'
+                  value={image}
+                  onChange={(e) => setImage(e.target.value)}
+                  className='tw-w-96 tw-pl-6 tw-py-2 tw-font-normal tw-text-green-700  focus:tw-border-teal-600 focus:tw-outline-none'></input>
+              </div>
+              <div>
+                <input
+                  type='file'
+                  // id='image-file'
+                  label='Choose File'
+                  // custom
+                  onChange={uploadFileHandler}
+                  className='tw-w-96 tw-pl-6 tw-mb-3 tw-font-normal tw-text-gray-700 tw-rounded  focus:tw-border-teal-600 focus:tw-outline-none'></input>
+                {uploading && <Loader />}
+              </div>
+            </div>
+            <div>
+              <label>Brand</label>
+              <br />
+              <input
                 type='text'
                 placeholder='Enter brand'
                 value={brand}
-                onChange={(e) => setBrand(e.target.value)}></Form.Control>
-            </Form.Group>
+                onChange={(e) => setBrand(e.target.value)}
+                className='tw-w-96 tw-mb-3 tw-px-4 tw-py-2 tw-font-normal tw-text-gray-700 tw-border tw-border-solid tw-border-gray-300 tw-rounded  focus:tw-border-teal-600 focus:tw-outline-none'></input>
+            </div>
 
-            <Form.Group controlId='countInStock'>
-              <Form.Label>Count In Stock</Form.Label>
-              <Form.Control
+            <div>
+              <label>Count In Stock</label>
+              <br />
+              <input
                 type='number'
                 placeholder='Enter countInStock'
                 value={countInStock}
-                onChange={(e) =>
-                  setCountInStock(e.target.value)
-                }></Form.Control>
-            </Form.Group>
+                onChange={(e) => setCountInStock(e.target.value)}
+                className='tw-w-96 tw-mb-3 tw-px-4 tw-py-2 tw-font-normal tw-text-gray-700 tw-border tw-border-solid tw-border-gray-300 tw-rounded  focus:tw-border-teal-600 focus:tw-outline-none'></input>
+            </div>
 
-            <Form.Group controlId='category'>
-              <Form.Label>Category</Form.Label>
-              <Form.Control
+            <div>
+              <label>Category</label>
+              <br />
+              <input
                 type='text'
                 placeholder='Enter category'
                 value={category}
-                onChange={(e) => setCategory(e.target.value)}></Form.Control>
-            </Form.Group>
+                onChange={(e) => setCategory(e.target.value)}
+                className='tw-w-96 tw-mb-3 tw-px-4 tw-py-2 tw-font-normal tw-text-gray-700 tw-border tw-border-solid tw-border-gray-300 tw-rounded  focus:tw-border-teal-600 focus:tw-outline-none'></input>
+            </div>
 
-            <Form.Group controlId='description'>
-              <Form.Label>Description</Form.Label>
-              <Form.Control
-                type='text'
+            <div>
+              <label>Description</label>
+              <br />
+              <textarea
+                rows='4'
+                cols='50'
                 placeholder='Enter description'
                 value={description}
-                onChange={(e) => setDescription(e.target.value)}></Form.Control>
-            </Form.Group>
+                onChange={(e) => setDescription(e.target.value)}
+                className='tw-w-96 tw-mb-3 tw-px-4 tw-py-2 tw-font-normal tw-text-gray-700 tw-border tw-border-solid tw-border-gray-300 tw-rounded  focus:tw-border-teal-600 focus:tw-outline-none'></textarea>
+            </div>
 
-            <Button type='submit' variant='primary'>
-              Update
-            </Button>
-          </Form>
+            <AppButton type='submit'>Update</AppButton>
+          </form>
+
+          // <Form onSubmit={submitHandler}>
+          //   <Form.Group controlId='name'>
+          //     <Form.Label>Name</Form.Label>
+          //     <Form.Control
+          //       type='name'
+          //       placeholder='Enter name'
+          //       value={name}
+          //       onChange={(e) => setName(e.target.value)}></Form.Control>
+          //   </Form.Group>
+
+          //   <Form.Group controlId='price'>
+          //     <Form.Label>Price</Form.Label>
+          //     <Form.Control
+          //       type='number'
+          //       placeholder='Enter price'
+          //       value={price}
+          //       onChange={(e) => setPrice(e.target.value)}></Form.Control>
+          //   </Form.Group>
+
+          //   <Form.Group controlId='image'>
+          //     <Form.Label>Image</Form.Label>
+          //     <Form.Control
+          //       type='text'
+          //       placeholder='Enter image url'
+          //       value={image}
+          //       onChange={(e) => setImage(e.target.value)}></Form.Control>
+          //     <Form.Control
+          //       type='file'
+          //       // id='image-file'
+          //       label='Choose File'
+          //       // custom
+          //       onChange={uploadFileHandler}></Form.Control>
+          //     {uploading && <Loader />}
+          //   </Form.Group>
+
+          //   <Form.Group controlId='brand'>
+          //     <Form.Label>Brand</Form.Label>
+          //     <Form.Control
+          //       type='text'
+          //       placeholder='Enter brand'
+          //       value={brand}
+          //       onChange={(e) => setBrand(e.target.value)}></Form.Control>
+          //   </Form.Group>
+
+          //   <Form.Group controlId='countInStock'>
+          //     <Form.Label>Count In Stock</Form.Label>
+          //     <Form.Control
+          //       type='number'
+          //       placeholder='Enter countInStock'
+          //       value={countInStock}
+          //       onChange={(e) =>
+          //         setCountInStock(e.target.value)
+          //       }></Form.Control>
+          //   </Form.Group>
+
+          //   <Form.Group controlId='category'>
+          //     <Form.Label>Category</Form.Label>
+          //     <Form.Control
+          //       type='text'
+          //       placeholder='Enter category'
+          //       value={category}
+          //       onChange={(e) => setCategory(e.target.value)}></Form.Control>
+          //   </Form.Group>
+
+          //   <Form.Group controlId='description'>
+          //     <Form.Label>Description</Form.Label>
+          //     <Form.Control
+          //       type='text'
+          //       placeholder='Enter description'
+          //       value={description}
+          //       onChange={(e) => setDescription(e.target.value)}></Form.Control>
+          //   </Form.Group>
+
+          //   <Button type='submit' variant='primary'>
+          //     Update
+          //   </Button>
+          // </Form>
         )}
-      </FormContainer>
+      </div>
     </>
   );
 };
